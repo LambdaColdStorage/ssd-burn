c052e405e8e2df96bf19a05eeab126fca18966bf
chuanli11 <cl.chuanli@gmail.com>
2019-04-22 13:22:19 -0700

    update

--- ssd_burn.sh
+++ ssd_burn.sh
@@ -3,8 +3,8 @@
 SSD_NAME=${1}
 NUM_ITER=${2:-10}
 
-./ssd_burn_seq_w.sh $NUM_ITER | tee "${SSD_NAME}_vertical_seq_w.txt"
-./ssd_burn_seq_r.sh $NUM_ITER | tee "${SSD_NAME}_vertical_seq_r.txt"
-./ssd_burn_rand_w.sh $NUM_ITER | tee "${SSD_NAME}_vertical_rand_w.txt"
-./ssd_burn_rand_r.sh $NUM_ITER | tee "${SSD_NAME}_vertical_rand_r.txt"
+./ssd_burn_seq_w.sh $NUM_ITER | tee "${SSD_NAME}_seq_w.txt"
+./ssd_burn_seq_r.sh $NUM_ITER | tee "${SSD_NAME}_seq_r.txt"
+./ssd_burn_rand_w.sh $NUM_ITER | tee "${SSD_NAME}_rand_w.txt"
+./ssd_burn_rand_r.sh $NUM_ITER | tee "${SSD_NAME}_rand_r.txt"
 
